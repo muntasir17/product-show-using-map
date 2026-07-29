cbb0895b946f7ef3f8a41003738d233d865cdacd
muntasir17 <muntasirshuvo791@gmail.com>
2026-07-29 10:37:31 +0600

    hello

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-
+const watchList = [
+  {
+    id: 1,
+    name: "KRONOS Pro Ultra",
+    price: "$1,000",
+    rating: 4.9,
+    buyMessage: "Buy Now",
+    photoUrl: "https://images.unsplash.com/photo-1508685096489-7aacd43bd3b1?q=80&w=600"
+  },
+  {
+    id: 2,
+    name: "KRONOS Stealth",
+    price: "$499",
+    rating: 4.7,
+    buyMessage: "Buy Now",
+    photoUrl: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?q=80&w=600"
+  },
+  {
+    id: 3,
+    name: "KRONOS Apex",
+    price: "$750",
+    rating: 4.8,
+    buyMessage: "Buy Now",
+    photoUrl: "https://images.unsplash.com/photo-1542496658-e33a6d0d50f6?q=80&w=600"
+  }
+];
 
 
 
